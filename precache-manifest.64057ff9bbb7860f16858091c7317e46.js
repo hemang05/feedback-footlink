7b2391b9c0a7cb8cccd450aad88e47ea78f05532
fcubedashboard <murtaza.sajawala@thefootballlink.com>
2020-10-23 16:41:52 +0530

    Updates

---
+++ precache-manifest.64057ff9bbb7860f16858091c7317e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db085e459bc1cb49f248566fc4e72c5",
+    "url": "/feedback-footlink/index.html"
+  },
+  {
+    "revision": "6e0f57613d2a21630405",
+    "url": "/feedback-footlink/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6690998ec209e1a10450",
+    "url": "/feedback-footlink/static/js/2.78d69b6b.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/feedback-footlink/static/js/2.78d69b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0f57613d2a21630405",
+    "url": "/feedback-footlink/static/js/main.bbd34597.chunk.js"
+  },
+  {
+    "revision": "dd98c9fe8ff5a2ec4620",
+    "url": "/feedback-footlink/static/js/runtime-main.d8429f1e.js"
+  }
+]);
